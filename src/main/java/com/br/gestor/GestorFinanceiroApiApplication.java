27b10a6b04d5c1b8b17fa2086102f354c4e6a94f
guigaon <58703086+guigaon@users.noreply.github.com>
2022-03-24 07:36:14 -0300

    alterando pom para deploy

--- src/main/java/com/br/gestor/GestorFinanceiroApiApplication.java
+++ src/main/java/com/br/gestor/GestorFinanceiroApiApplication.java
@@ -2,12 +2,8 @@ package com.br.gestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.br.gestor.config.property.GestorFinanceiroApiProperty;
 
 @SpringBootApplication
-@EnableConfigurationProperties(GestorFinanceiroApiProperty.class)
 public class GestorFinanceiroApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/br/gestor/config/property/GestorFinanceiroApiProperty.java
+++ src/main/java/com/br/gestor/config/property/GestorFinanceiroApiProperty.java
@@ -3,31 +3,28 @@ package com.br.gestor.config.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties("gestorFinanceiro")
+@ConfigurationProperties("gestorfinanceiro")
 @Component
 public class GestorFinanceiroApiProperty {
 
 	private String originPermitida = "http://localhost:8000";
-	
+
+	private final Seguranca seguranca = new Seguranca();
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
 	public String getOriginPermitida() {
 		return originPermitida;
 	}
-
 
 	public void setOriginPermitida(String originPermitida) {
 		this.originPermitida = originPermitida;
 	}
 
+	public static class Seguranca {
 
-	private final Seguranca seguranca = new Seguranca();
-	
-	public Seguranca getSeguranca() {
-		return seguranca;
-	}
-
-
-	public static class Seguranca{
-		
 		private boolean enableHttps;
 
 		public boolean isEnableHttps() {
@@ -37,8 +34,7 @@ public class GestorFinanceiroApiProperty {
 		public void setEnableHttps(boolean enableHttps) {
 			this.enableHttps = enableHttps;
 		}
-		
-	
-	
+
 	}
+		
 }
